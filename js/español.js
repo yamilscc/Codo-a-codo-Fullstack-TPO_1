fd77d94cdc77a8f97b3a1c39f8d346ff28525324
yamil <yamilscc@outlook.com>
2024-05-31 05:57:29 -0300

    Cambio

--- js/español.js
+++ js/español.js
@@ -1,16 +1,5 @@
 
-
-// fetch("https://yamilscc.github.io/Codo-a-codo-Fullstack-TPO_1/cursos.json")
-// .then(response => response.json())
-// .then(data => {
-//     // Procesamiento de la info que llega de la API
-//     console.log(data + "HOLA");
-//     })
-// .catch(error => console.log("Ocurrió un error! " + error));
-
-fetch("https://randomuser.me/api") //Hacemos un get (petición http)desde el endpoint para obtener el JSON
-
-//PROMESAS (then's) EN JS. Es un concepto complejo de JS que esta asociado con ASINCRONIA / EVENTOS ASINCRONICOS
+fetch("https://yamilscc.github.io/Codo-a-codo-Fullstack-TPO_1/src/json/cursos.json") //Hacemos un get (petición http)desde el endpoint para obtener el JSON
 .then(response => response.json())
 .then(data => {
     // Procesamiento de la info que llega de la API
