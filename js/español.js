051967423c0e810985bcf473d071f3d46417a06b
yamil <yamilscc@outlook.com>
2024-05-31 08:46:04 -0300

    Cambios

--- js/español.js
+++ js/español.js
@@ -4,10 +4,14 @@ const url = `https://yamilscc.github.io/Codo-a-codo-Fullstack-TPO_1/src/json/cur
 
 
 
-let original = document.querySelector("#Plantilla");
-let referencia = original.cloneNode(true);
+let tabla = document.querySelector("#Plantilla");
+let contenedor = document.querySelector("#Contenedor");
 
-original.remove(); //Removemos el original y nos quedamos con la plantilla en copia
+let copiaTabla = tabla.cloneNode(true);
+
+console.log(copiaTabla);
+
+tabla.remove(); //Removemos el original y nos quedamos con la plantilla en copia
 
 fetch(url) //Hacemos un get (petición http)desde el endpoint para obtener el JSON
 .then(response => response.json())
@@ -15,16 +19,56 @@ fetch(url) //Hacemos un get (petición http)desde el endpoint para obtener el JS
     // Procesamiento de la info que llega de la API
     console.log(data.cursos_español);
 
-    referencia.querySelector('td:nth-child(1)').textContent = data.cursos_español['Tipo Curso'];
-    referencia.querySelector('td:nth-child(2)').textContent =  data.cursos_español['Curso'];
-    referencia.querySelector('td:nth-child(3)').textContent =  data.cursos_español['Profesor'];
-    referencia.querySelector('td:nth-child(4)').textContent =  data.cursos_español['Nivel'];
-    referencia.querySelector('td:nth-child(5)').textContent =  data.cursos_español['Duración'];
+    // referencia.querySelector('td:nth-child(1)').textContent = data.cursos_español['Tipo Curso'];
+    // referencia.querySelector('td:nth-child(2)').textContent =  data.cursos_español['Curso'];
+    // referencia.querySelector('td:nth-child(3)').textContent =  data.cursos_español['Profesor'];
+    // referencia.querySelector('td:nth-child(4)').textContent =  data.cursos_español['Nivel'];
+    // referencia.querySelector('td:nth-child(5)').textContent =  data.cursos_español['Duración'];
 
-    console.log(data.cursos_español[1].Nivel);
+    console.log(data.cursos_español[1].Tipo_Curso);
 
-    data.cursos_español[1].Tipo
-    data.cursos_español[i].Nivel
+        for (let i = 0; i < data.cursos_español.length ; i++) {
+            copiaTabla.querySelector("#Tipo_Curso").textContent = data.cursos_español[i].Tipo_Curso;
+            copiaTabla.querySelector("#Curso").textContent = data.cursos_español[i].Curso;
+            copiaTabla.querySelector("#Profesor").textContent = data.cursos_español[i].Profesor;
+            copiaTabla.querySelector("#Nivel").textContent = data.cursos_español[i].Nivel;
+            copiaTabla.querySelector("#Duración").textContent = data.cursos_español[i].Duración;
+
+            contenedor.appendChild(copiaTabla);
+        }
+
+
+
+    // }
+
+    // referencia.insertRow();
+
+    // referencia.innerHTML = "<td>"+ data.cursos_español[1].Curso +"</td>"
+
+    // document.querySelector("#Plantilla").
+
+    // for (let i = 0; i < 8 ; i++) {
+        // console.log(data.cursos_español[i].Curso);
+        // console.log(data.cursos_español[i].Tipo_Curso);
+        // console.log(data.cursos_español[i].Profesor);
+    //     console.log(data.cursos_español[i].Nivel);
+    //     // console.log(data.cursos_español[i].Duración);
+
+    //     referencia.querySelector('td:nth-child(i+1)').textContent = data.cursos_español[i].Curso;
+
+    //     referencia.querySelector('td:nth-child(i+2)').textContent = data.cursos_español[i].Tipo_Curso;
+
+    //     referencia.querySelector('td:nth-child(i+3)').textContent = data.cursos_español[i].Profesor;
+
+    //     referencia.querySelector('td:nth-child(i+4)').textContent = data.cursos_español[i].Nivel;
+
+    //     referencia.querySelector('td:nth-child(i+5)').textContent = data.cursos_español[i].Duración;
+
+    //     document.querySelector("table").appendChild(referencia);
+    // }
+
+    
+    // // data.cursos_español[i].Nivel
 
 
     })
